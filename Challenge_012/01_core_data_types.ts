2363ed4b187c8c88c0c3ddb8e7b7f359a5c7ad40
Paul <wmdev0808@gmail.com>
2022-08-03 09:51:30 -0400

    -- Primer to TypeScript types

---
+++ Challenge_012/01_core_data_types.ts
@@ -0,0 +1,16 @@
+// number, string, boolean
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5; // 5.0
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
