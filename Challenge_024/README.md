a31c0310d010d46d213e282495afef99c9a3a1d5
Paul <wmdev0808@gmail.com>
2022-08-10 22:17:00 -0400

    -- Uses Google maps geocoding API -- Adds types for googlemaps -- Call Google maps Geocoding API using Axios

---
+++ Challenge_024/README.md
@@ -0,0 +1,3 @@
+# Project: Share A Place App
+
+- User enters an address, then shows its place on Google map
---
+++ Challenge_024/src/app.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "YOUR_API_KEY";
+
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const entertedAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        entertedAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
