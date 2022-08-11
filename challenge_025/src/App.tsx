fa666cb6c93da804676fe8a5fb893fc7f5ba1633
Paul <wmdev0808@gmail.com>
2022-08-10 23:40:59 -0400

    -- Builds a basic todo list app using create-react-app and TypeScript

---
+++ challenge_025/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+import "./App.css";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
